e6a0440c8003a95be579f18e7d910ef46c2dd30a
Mkps <alex.loubiere@gmail.com>
2024-04-03 05:12:57 +0200

    modified base parameters in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int	main()
 	NDArray Y = {{6.0, 3.0}};
 	NDArray initialWeights = {{0.2, 0.1, 0.6}};
 	LinearRegression l(initialWeights, 0.001, X.size(), X, Y);
-	l.trainModel(4000000);
+	l.trainModel(1000);
 	return (0);
 }
